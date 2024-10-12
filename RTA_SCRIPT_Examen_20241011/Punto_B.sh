b326bd6414fa8804d9bdd8dc2a6c4bd85fdddefb
LucianaRadimak <123275101+LucianaRadimak@users.noreply.github.com>
2024-10-12 14:02:32 -0300

    Update Punto_B.sh

--- RTA_SCRIPT_Examen_20241011/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241011/Punto_B.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
-sudo parted /dev/sdc --script mklabel gpt
+
+DISK="/dev/sdc"
+MOUNT_DIR="/home/vagrant"
+PART_SIZE=1
+
+sudo parted $DISK --script mklabel gpt
+
 for i in {1..3}; do
-  sudo parted /dev/sdc --script mkpart primary ext4 1GB $((1 + i))GB
+  START=$(( (i - 1) * PART_SIZE ))
+  END=$(( START + PART_SIZE ))
+  sudo parted $DISK --script mkpart primary ext4 ${START}GB ${END}GB
 done
-for i in {1..8}; do
-  if (( i == 1 )); then
-    sudo parted /dev/sdc --script mkpart extended 4GB 10GB
-  fi
-  sudo parted /dev/sdc --script mkpart logical ext4 $((4 + i - 1))GB $((4 + i))GB
+
+sudo parted $DISK --script mkpart extended 3GB 10GB
+
+for i in {1..7}; do
+  START=$(( 3 + (i - 1) * PART_SIZE ))
+  END=$(( START + PART_SIZE ))
+  sudo parted $DISK --script mkpart logical ext4 ${START}GB ${END}GB
 done
-sudo fdisk -l /dev/sdc
+
+sudo fdisk -l $DISK
+
 for i in {1..3}; do
-  sudo mkfs -t ext4 /dev/sdc$i
+  sudo mkfs -t ext4 ${DISK}${i}
 done
 
 for i in {5..11}; do
-  sudo mkfs -t ext4 /dev/sdc$i
+  sudo mkfs -t ext4 ${DISK}${i}
 done
+
 echo "/dev/sdc1 /home/vagrant/alumno_1/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
 echo "/dev/sdc2 /home/vagrant/alumno_1/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
 echo "/dev/sdc3 /home/vagrant/alumno_1/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
@@ -27,5 +40,7 @@ echo "/dev/sdc8 /home/vagrant/alumno_3/parcial_1 ext4 defaults 0 0" | sudo tee -
 echo "/dev/sdc9 /home/vagrant/alumno_3/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
 echo "/dev/sdc10 /home/vagrant/alumno_3/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
 echo "/dev/sdc11 /home/vagrant/profesores ext4 defaults 0 0" | sudo tee -a /etc/fstab
+
 sudo mount -a
+
 df -h
